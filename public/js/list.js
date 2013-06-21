734498b7932c91cbd91199936eb0daa49ea941bc
Olof Olsson <olof.d04oo@gmail.com>
2013-06-21 10:57:35 +0200

    first commit

---
+++ public/js/list.js
@@ -0,0 +1,8 @@
+// Here's my data model
+function ListViewModel() {
+    self = this;
+
+    
+};
+
+ko.applyBindings(new ListViewModel());
---
+++ routes/api.js
@@ -0,0 +1,12 @@
+
+// var list = require('../models/list');
+
+// exports.tasks = function(req, res){
+// 	console.log(req.body);
+//   	process.nextTick(function(){
+// 		var query = task.find({'fbId': req.user.fbId});
+// 		query.exec(function(err, tasks){
+// 			res.send(tasks);
+// 		});
+// 	});
+// };
